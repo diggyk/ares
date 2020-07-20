76b4d55df49f789839b0a1a2b1727792430af5be
Digant C Kasundra <diggyk@gmail.com>
2020-07-20 13:09:55 -0700

    add flag for valuables

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -12,59 +12,85 @@ pub struct ServerConfig {
 
     // maximum number of robots to spawn
     max_bots: usize,
-    // how quickly to regen robots
-    // regen_rate: usize,
-} 
+
+    // maximum number of valuables files
+    max_valuables: usize,
+}
 
 pub fn get_config() -> ServerConfig {
     let matches = App::new("Ares Grid Admin")
         .version("0.1.0")
         .about("Create/maintain grids")
-        .arg(Arg::with_name("dbuser")
-            .short("u")
-            .long("user")
-            .takes_value(true)
-            .help("Database username"))
-        .arg(Arg::with_name("dbpw")
-            .short("p")
-            .long("password")
-            .takes_value(true)
-            .help("Database password"))
-        .arg(Arg::with_name("dbhost")
-            .short("o")
-            .long("hostname")
-            .takes_value(true)
-            .help("Database hostname"))
-        .arg(Arg::with_name("db")
-            .short("n")
-            .long("dbname")
-            .takes_value(true)
-            .help("Database name"))
-        .arg(Arg::with_name("max_bots")
-            .required(true)
-            .takes_value(true)
-            .help("Maximum number of robots"))
-        .arg(Arg::with_name("regen_rate")
-            .required(true)
-            .takes_value(true)
-            .help("How often to spawn a robot (in seconds)"))
+        .arg(
+            Arg::with_name("dbuser")
+                .short("u")
+                .long("user")
+                .takes_value(true)
+                .help("Database username"),
+        )
+        .arg(
+            Arg::with_name("dbpw")
+                .short("p")
+                .long("password")
+                .takes_value(true)
+                .help("Database password"),
+        )
+        .arg(
+            Arg::with_name("dbhost")
+                .short("o")
+                .long("hostname")
+                .takes_value(true)
+                .help("Database hostname"),
+        )
+        .arg(
+            Arg::with_name("db")
+                .short("n")
+                .long("dbname")
+                .takes_value(true)
+                .help("Database name"),
+        )
+        .arg(
+            Arg::with_name("max_bots")
+                .required(true)
+                .takes_value(true)
+                .help("Maximum number of robots"),
+        )
+        .arg(
+            Arg::with_name("max_valuables")
+                .required(true)
+                .takes_value(true)
+                .help("How many valuables piles to keep"),
+        )
         .get_matches();
 
-        let dbuser = matches.value_of("dbuser").unwrap_or("ares").to_string();
-        let dbpw = matches.value_of("dbpw").unwrap_or("ares").to_string();
-        let dbhost = matches.value_of("dbhost").unwrap_or("localhost").to_string();
-        let dbname = matches.value_of("db").unwrap_or("ares").to_string();
+    let dbuser = matches.value_of("dbuser").unwrap_or("ares").to_string();
+    let dbpw = matches.value_of("dbpw").unwrap_or("ares").to_string();
+    let dbhost = matches
+        .value_of("dbhost")
+        .unwrap_or("localhost")
+        .to_string();
+    let dbname = matches.value_of("db").unwrap_or("ares").to_string();
 
-        let max_bots = matches.value_of("max_bots").unwrap_or("10");
-        let max_bots = max_bots.parse::<usize>().expect("Could not parse max bots");
+    let max_bots = matches.value_of("max_bots").unwrap_or("10");
+    let max_bots = max_bots.parse::<usize>().expect("Could not parse max bots");
 
-        // let regen_rate = matches.value_of("regen_rate").unwrap_or("1");
-        // let regen_rate = regen_rate.parse::<usize>().expect("Could not parse regen rate");
+    let max_valuables = matches.value_of("max_valuables").unwrap_or("1");
+    let max_valuables = max_valuables
+        .parse::<usize>()
+        .expect("Could not parse max valuables");
 
-        let dbconfig = DbConfig{dbuser, dbpw, dbhost, dbname};
-        let conn = establish_connection(&dbconfig);
+    let dbconfig = DbConfig {
+        dbuser,
+        dbpw,
+        dbhost,
+        dbname,
+    };
+    let conn = establish_connection(&dbconfig);
 
-        ServerConfig {
-            dbconfig, conn, max_bots,
-        }
+    ServerConfig {
+        dbconfig,
+        conn,
+        max_bots,
+        max_valuables,
+    }
 }
